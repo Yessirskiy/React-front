16d37f84c134e1dbf170fa12299d0f256d2ee43b
Nikolai Dobrydnev <dobrydnevni@gmail.com>
2024-08-09 22:49:35 +0300

    Made user profile responsive

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -32,68 +32,75 @@ const ProfileForm = ({themeConfig}) => {
                     form={changeProfileForm}
                     layout="vertical"
                     onFinish={onFinish}
-                    style={{backgroundColor: themeConfig.token.colorBgContainer }}
                     variant='filled'
                 >
-                    <Row>
-                    <Col className="mr-8 text-center">
-                        <Image
-                            width={200}
-                            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-                            style={{borderRadius: themeConfig.token.borderRadiusLG}}
-                        />
-                        <Form.Item name="avatar" valuePropName="fileList">
-                            <Upload listType="picture" maxCount={1}>
-                            <Button icon={<UploadOutlined />} className="mt-5 h-10" style={{
-                                borderRadius: themeConfig.token.borderRadius, 
-                                width: "200px"}}>
-                                Выбрать файл...
-                            </Button>
-                            </Upload>
-                        </Form.Item>
-                    </Col>
-                    <Col style={{ flex: 1 }}>
-                        <Form.Item
-                            label="Имя"
-                            name="firstName"
-                            rules={[{ required: true, message: 'Введите ваше имя' }]}
-                        >
-                            <Input placeholder="Введите ваше имя" size='large'/>
-                        </Form.Item>
+                    <Row
+                    gutter={[24, 24]}
+                    >
+                        <Col 
+                        className="text-center gutter-row"
+                        xs={24} sm={24} md={10}
+                        lg={8} xl={4}>
+                            <Image
+                                // width={200}
+                                src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+                                style={{borderRadius: themeConfig.token.borderRadiusLG}}
+                            />
+                            <Form.Item name="avatar" valuePropName="fileList">
+                                <Upload listType="picture" maxCount={1}>
+                                <Button icon={<UploadOutlined />} className="mt-5 h-10" style={{
+                                    borderRadius: themeConfig.token.borderRadius, 
+                                    width: '100%'}}>
+                                    Выбрать файл...
+                                </Button>
+                                </Upload>
+                            </Form.Item>
+                        </Col>
+                        <Col 
+                        className="gutter-row"
+                        xs={24} sm={24} md={14}
+                        lg={16} xl={20}>
+                            <Form.Item
+                                label="Имя"
+                                name="firstName"
+                                rules={[{ required: true, message: 'Введите ваше имя' }]}
+                            >
+                                <Input placeholder="Введите ваше имя" size='large'/>
+                            </Form.Item>
 
-                        <Form.Item
-                            label="Фамилия"
-                            name="lastName"
-                            rules={[{ message: 'Введите вашу фамилию' }]}
-                        >
-                            <Input placeholder="Введите вашу фамилию" size='large' />
-                        </Form.Item>
+                            <Form.Item
+                                label="Фамилия"
+                                name="lastName"
+                                rules={[{ message: 'Введите вашу фамилию' }]}
+                            >
+                                <Input placeholder="Введите вашу фамилию" size='large' />
+                            </Form.Item>
 
-                        <Form.Item
-                            label="Почта"
-                            name="email"
-                            rules={[{required: true, message: 'Введите вашу почту'}]}
-                            initialValue="admin@example.com"
-                        >
-                            <Input size='large'/>
-                        </Form.Item>
+                            <Form.Item
+                                label="Почта"
+                                name="email"
+                                rules={[{required: true, message: 'Введите вашу почту'}]}
+                                initialValue="admin@example.com"
+                            >
+                                <Input size='large'/>
+                            </Form.Item>
 
-                        <Form.Item
-                            label="Номер телефона"
-                            name="phone"
-                            rules={[{ message: 'Введите номер телефона' }]}
-                        >
-                            <Input placeholder="Введите номер телефона" size='large'/>
-                        </Form.Item>
+                            <Form.Item
+                                label="Номер телефона"
+                                name="phone"
+                                rules={[{ message: 'Введите номер телефона' }]}
+                            >
+                                <Input placeholder="Введите номер телефона" size='large'/>
+                            </Form.Item>
 
-                        <Form.Item className="m-0">
-                            <div  style={{ textAlign: 'right' }}>
-                            <Button type="primary" htmlType="submit" size='large'>
-                                Сохранить
-                            </Button>
-                            </div>
-                        </Form.Item>
-                    </Col>
+                            <Form.Item className="m-0">
+                                <div  style={{ textAlign: 'right' }}>
+                                <Button type="primary" htmlType="submit" size='large'>
+                                    Сохранить
+                                </Button>
+                                </div>
+                            </Form.Item>
+                        </Col>
                     </Row>
                 </Form>
             </div>
