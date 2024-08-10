27fdef9b9dbbc98c2ddea57607c778d8aa2aac8e
Nikolai Dobrydnev <dobrydnevni@gmail.com>
2024-08-10 22:56:58 +0300

    Account deactivation done. User settings done.

--- src/pages/UserSettings.jsx
+++ src/pages/UserSettings.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {useState} from 'react';
 
+import { WarningOutlined } from '@ant-design/icons';
 import { Space, ConfigProvider } from 'antd';
-import { Form, Input, Upload, Button, Switch, Segmented} from 'antd';
-import {Row, Col, Divider} from 'antd';
+import { Form, Input, Upload, Button, Checkbox, Segmented} from 'antd';
+import {Row, Col, Divider, Alert} from 'antd';
 import { UserOutlined, UploadOutlined } from '@ant-design/icons';
 
 const SettingsForm = ({themeConfig}) => {
@@ -12,12 +13,6 @@ const SettingsForm = ({themeConfig}) => {
 
   const onFinish = (values) => {
     console.log('Form values: ', values);
-  };
-  
-  const cardStyling = {
-    padding: "24px",
-    backgroundColor: themeConfig.token.colorBgContainer,
-    borderRadius: themeConfig.token.borderRadiusLG,
   };
 
   return (
@@ -116,7 +111,7 @@ const SettingsForm = ({themeConfig}) => {
 
                             <Form.Item className="m-0">
                                 <div  style={{ textAlign: 'right' }}>
-                                <Button type="primary" htmlType="submit" className="h-10">
+                                <Button type="primary" htmlType="submit" size='middle'>
                                     Сохранить
                                 </Button>
                                 </div>
@@ -218,7 +213,7 @@ const SettingsForm = ({themeConfig}) => {
 
                             <Form.Item className="m-0">
                                 <div  style={{ textAlign: 'right' }}>
-                                <Button type="primary" htmlType="submit" className="h-10">
+                                <Button type="primary" htmlType="submit" size='middle'>
                                     Сохранить
                                 </Button>
                                 </div>
@@ -227,9 +222,9 @@ const SettingsForm = ({themeConfig}) => {
                     </Row>
                 </Form>
             </div>
-            <Divider orientation="left">Настройки конфиденциальности</Divider>
+            <Divider orientation="left">Деактивация аккаунта</Divider>
             <div 
-                className="p-6" 
+                className="p-6 mb-8" 
                 style={{ 
                 borderRadius: themeConfig.token.borderRadiusLG,
                 backgroundColor: themeConfig.token.colorBgContainer 
@@ -241,92 +236,22 @@ const SettingsForm = ({themeConfig}) => {
                     onFinish={onFinish}
                     variant='filled'
                 >
-                    <Row 
-                        gutter={[30, 0]}
-                    >
-                        <Col 
-                            className="gutter-row"
-                            xs={24} sm={24} md={24}
-                            lg={24} xl={12}
-                        >
-                            <Form.Item
-                                label="Моя фотография видна"
-                                name="notify_login"
-                                layout='horizontal'
-                                valuePropName="checked"
-                            >
-                                <div className='text-right'>
-                                    <Segmented
-                                        options={['Мне', 'Друзьям', 'Всем',]}
-                                        onChange={(value) => {
-                                        console.log(value);
-                                        }}
-                                    />
-                                </div>
-                            </Form.Item>
-
-                            <Form.Item
-                                label="Моя фамилия видна"
-                                name="notify_login"
-                                layout='horizontal'
-                                valuePropName="checked"
-                            >
-                                <div className='text-right'>
-                                    <Segmented
-                                        options={['Мне', 'Друзьям', 'Всем',]}
-                                        onChange={(value) => {
-                                        console.log(value);
-                                        }}
-                                    />
-                                </div>
-                            </Form.Item>
-                        </Col>
-                        <Col 
-                            className="gutter-row"
-                            xs={24} sm={24} md={24}
-                            lg={24} xl={12}
-                        >
-                            <Form.Item
-                                label="Моя почта видна"
-                                name="notify_login"
-                                layout='horizontal'
-                                valuePropName="checked"
-                            >
-                                <div className='text-right'>
-                                    <Segmented
-                                        options={['Мне', 'Друзьям', 'Всем',]}
-                                        onChange={(value) => {
-                                        console.log(value);
-                                        }}
-                                    />
-                                </div>
-                            </Form.Item>
-
-                            <Form.Item
-                                label="Мой телефон виден"
-                                name="notify_login"
-                                layout='horizontal'
-                                valuePropName="checked"
-                            >
-                                <div className='text-right'>
-                                    <Segmented
-                                        options={['Мне', 'Друзьям', 'Всем',]}
-                                        onChange={(value) => {
-                                        console.log(value);
-                                        }}
-                                    />
-                                </div>
-                            </Form.Item>
-
-                            <Form.Item className="m-0">
-                                <div  style={{ textAlign: 'right' }}>
-                                <Button type="primary" htmlType="submit" className="h-10">
-                                    Сохранить
-                                </Button>
-                                </div>
-                            </Form.Item>
-                        </Col>
-                    </Row>
+                    <Alert
+                        className='mb-4'
+                        message="Вы деактивируете свой аккаунт"
+                        description="Для дополнительной безопасности, рекомендуем ознакомиться с последствиями деактивации аккаунта."
+                        type="warning"
+                        icon={<WarningOutlined/>}
+                        showIcon
+                    />
+                    <Form.Item className="mb-4">
+                        <Checkbox>Я подтверждаю деактивацию аккаунта.</Checkbox>
+                    </Form.Item>
+                    <Form.Item className="mb-0">
+                        <Button type="primary" danger size='middle'>
+                            Деактивировать
+                        </Button>
+                    </Form.Item>
                 </Form>
             </div>
         </Space>
