8ed624a4d5e532b1b633aa525a2d5adfa882744b
Nikolai Dobrydnev <dobrydnevni@gmail.com>
2024-09-08 22:48:04 +0300

    Courses application api calls

--- src/api/courses.js
+++ src/api/courses.js
@@ -1,5 +1,7 @@
 const courseURL = 'api/courses/'
 const feedURL = 'api/courses/feed/'
+const applyURL = 'api/courses/apply/'
+const applicationURL = 'api/courses/application/'
 
 export const getCourse = async (api, id) => {
     try {
@@ -23,6 +25,30 @@ export const getCoursesFeed = async (api, page, page_size, params) => {
         return response.data;
     } catch (error) {
         console.log("Error while gettings courses feed:", error);
+        throw error;
+    }
+}
+
+export const courseApply = async (api, comment, applicant_id, course_id) => {
+    try {
+        const response = await api.post(applyURL, {
+            comment: comment,
+            applicant: applicant_id,
+            course: course_id,
+        });
+        return response.data;
+    } catch (error) {
+        console.log("Error while applying for the course:", error);
+        throw error;
+    }
+}
+
+export const getCourseApplication = async (api, application_id) => {
+    try {
+        const response = await api.get(`${applicationURL}${application_id}/`);
+        return response.data;
+    } catch (error) {
+        console.log("Error while getting course application:", error);
         throw error;
     }
 }
